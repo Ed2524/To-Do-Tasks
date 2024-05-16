4c2b79c337db3f1b5ffcb42ebe07e395daa7e96a
Ed2524 <166954551+Ed2524@users.noreply.github.com>
2024-05-16 20:44:16 +0400

    Add files via upload

---
+++ to,do,task.py
@@ -0,0 +1,43 @@
+# tasks = {}
+
+# def add_task ():
+#     task_name = input("Say your task name: ")
+#     task_priority = input("Say the priority of your task: High/Medium/low : ")
+#     tasks[task_name] = task_priority
+#     print(f"Task name:{task_name}, priority {task_priority}")
+    
+
+
+# def view_tasks ():
+#     if not tasks:
+#         print("There's nothing to see:")
+#     for key, value in tasks.items():
+#         print(key, value)
+
+# def remove_task ():
+#     task_deleting = input("Which task do you want to delete? ")
+#     if task_deleting in tasks:
+#         del task_deleting
+#     else:
+#         print("There's no task with this name")
+
+
+
+
+
+# while True:
+#     print("Task Manegment System:")
+#     print("1. Add task:")
+#     print("2. View tasks:")
+#     print("3. Remove task:")
+#     print("4. Exit")
+#     x = input("Enter your choice (1, 2, 3 or 4): ")
+#     if x == '1':
+#         add_task()
+#     elif x == '2':
+#         view_tasks()
+#     elif x == '3':
+#         remove_task()
+#     else:
+#         break
+
